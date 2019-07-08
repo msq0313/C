6c17753f3b5f56f5b41309f9de19719589a8a080
msq0313 <513299488@qq.com>
2019-07-09 00:50:34 +0800

    链表

---
+++ 0707node/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "node.h"
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+//	typedef struct _node{
+//	int value;
+//	struct _node *next;
+//	}Node;
+int main(int argc, char *argv[]) {
+	int number;
+	int x=y=10;
+	do{
+		scanf("%d",&number);
+		if(number != -1){
+			
+		}
+	}while(number != -1);
+	return 0;
+}
